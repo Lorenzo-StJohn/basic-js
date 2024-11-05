3b212695cea16e5dbc5cc1639d8530df74e01c11
Lorenzo-StJohn <britishaccent@bk.ru>
2024-11-06 03:50:43 +0400

    feat: implement calculateHanoi function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,24 +1,35 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  let pow = 1;
+  let temp = 2;
+  let num = disksNumber;
+  while (num > 0) {
+    if (num % 2) pow *= temp;
+    temp *= temp;
+    num -= num % 2;
+    num /= 2;
+  }
+  return {
+    turns: pow - 1,
+    seconds: Math.floor(((pow - 1) / turnsSpeed) * 3600),
+  };
 }
 
 module.exports = {
-  calculateHanoi
+  calculateHanoi,
 };
