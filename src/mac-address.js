e24c7f8acc98043322ea71175e84b2e8dbe5c399
Lorenzo-StJohn <britishaccent@bk.ru>
2024-11-06 22:29:53 +0400

    feat: deleteDigit function

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,10 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  //'00-1B-63-84-45-E6'
+  if (n.length !== 6 * 2 + 5) return false;
+  for (let i = 2; i < n.length; i += 3) {
+    if (n[i] !== "-") return false;
+  }
+  for (let i = 0; i < n.length; i += 3) {
+    if (!(n[i] >= "0" && n[i] <= "9") && !(n[i] >= "A" && n[i] <= "F"))
+      return false;
+  }
+  return true;
 }
+
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
