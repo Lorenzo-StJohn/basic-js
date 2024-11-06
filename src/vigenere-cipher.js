f2143c7ac318cc023fe37125f102dd9a27d0b45c
Lorenzo-StJohn <britishaccent@bk.ru>
2024-11-06 21:23:28 +0400

    feat: implement VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,70 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(t = true) {
+    this.type = t;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (typeof key === "undefined" || typeof message === "undefined")
+      throw new Error("Incorrect arguments!");
+    let ans = "";
+    let temp;
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+    let j = 0;
+    for (let i = 0; i < message.length; i += 1) {
+      if (message[i] >= "A" && message[i] <= "Z") {
+        temp = message.charCodeAt(i) + key.charCodeAt(j) - "A".charCodeAt(0);
+        if (temp > "Z".charCodeAt(0)) temp -= 26;
+        ans += String.fromCharCode(temp);
+        ++j;
+        if (j === key.length) j = 0;
+      } else ans += message[i];
+    }
+    if (!this.type) ans = ans.split("").reverse().join("");
+    return ans;
+  }
+  decrypt(message, key) {
+    if (typeof key === "undefined" || typeof message === "undefined")
+      throw new Error("Incorrect arguments!");
+    let ans = "";
+    let temp;
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+    let j = 0;
+    for (let i = 0; i < message.length; i += 1) {
+      if (message[i] >= "A" && message[i] <= "Z") {
+        temp = message.charCodeAt(i) - key.charCodeAt(j) + "A".charCodeAt(0);
+        if (temp < "A".charCodeAt(0)) temp += 26;
+        ans += String.fromCharCode(temp);
+        ++j;
+        if (j === key.length) j = 0;
+      } else ans += message[i];
+    }
+    if (!this.type) ans = ans.split("").reverse().join("");
+    return ans;
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
